5630b29a6df6758f101f5dcae1f1599b11fe4dda
SethBlondeel <127240348+SethBlondeel@users.noreply.github.com>
2023-03-21 19:44:56 +0100

    main.ts bijwerken

--- main.ts
+++ main.ts
@@ -3,12 +3,13 @@ let opKnopGedrukt = false
 let spelBezig = false
 let positieMol : number
 let startTijd : number
+
 input.onGesture(Gesture.Shake, function () {
     basic.showString("3-2-1")
     spelBezig = true
-})
 while (spelBezig) {
     positieMol = randint(0,1)
+    opKnopGedrukt = false
     basic.pause(300)
     if (positieMol = 0) {
         basic.showLeds(`
@@ -28,30 +29,33 @@ while (spelBezig) {
             . . . . #
             `)
     }
-    input.runningTime ()
     startTijd = input.runningTime()
-    while (opKnopGedrukt = false) {    
+    while (opKnopGedrukt == false) {    
         if (input.runningTime() - startTijd > (1000)) {
+        opKnopGedrukt = true
         basic.showString("Game Over")
+        game.isGameOver()
         }
-    }
-    if (positieMol = 0 && input.buttonIsPressed(Button.A) ) {
+    if (positieMol == 0 && input.buttonIsPressed(Button.A) ) {
     opKnopGedrukt = true
     basic.showIcon(IconNames.Yes)
+    game.addScore(1)
     }
-    if (positieMol = 0 && input.buttonIsPressed(Button.B)) {
+    else if (positieMol == 0 && input.buttonIsPressed(Button.B)) {
     opKnopGedrukt = true
     basic.showString("Game Over")
-
+    game.isGameOver()
     }
-    if (positieMol = 1 && input.buttonIsPressed(Button.B)) {
-        opKnopGedrukt = true
-        basic.showString
-    }
-    if (positieMol = 1 && input.buttonIsPressed(Button.B)) {
+    if (positieMol == 1 && input.buttonIsPressed(Button.B)) {
         opKnopGedrukt = true
         basic.showIcon(IconNames.Yes)
-    
+        game.addScore(1)
+    }
+    else if (positieMol == 1 && input.buttonIsPressed(Button.A)) {
+        opKnopGedrukt = true
+        basic.showString("Game Over")
+        game.isGameOver()
     }
 }
-
+}
+})
